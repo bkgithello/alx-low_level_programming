9046107936753044c410db2a3d899a38f8d6c1a0
bkgighello <birukdark@gmail.com>
2022-10-18 08:06:48 -0700

    1 i sometimes suffer

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet -> print_alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+int i;
+for (i='a' i<='z'; i++)
+{
+_putchar(i);
+}
+-putchar('\n');
+
+}
