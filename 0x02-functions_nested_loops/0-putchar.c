9acd829cfdfaa6e4b7ff50edea658939a83480df
bkgighello <birukdark@gmail.com>
2022-10-18 07:43:43 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
-#include "holberton.h"
+#include <main.h>
 /**
- * main - Entry point
- * Return:Always 0
+ * main - prints putchar
+ * Retrun: always 0
  */
-int main(void){
-char *sh = "Holberton";
-while(*sh)
+int main(void)
 {
-_putchar(*sh);
-sh++;
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_puthcar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return (0);
 }
-_putchar('\n');
-return(0);
-}
+
