d55d0e689e54e10ad79b3cfd380f2b3dced04b99
bkgighello <birukdark@gmail.com>
2022-10-24 08:04:10 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+		printf("%d, ", a[i]);
+		if (i == (n - 1))
+			printf("%d", a[n - 1]);
+	printf("\n");
+}
+
+9-strcpy.c
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
