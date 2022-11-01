401774c17f2672df68520c61864f90e8e5169488
bkgighello <birukdark@gmail.com>
2022-10-31 22:14:50 -0700

    7-fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,11 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
-  * print_chessboard - Print the chessboard
-  * @a: array of pieces
-  *
-  * Return: Nothing.
-  */
+ * print_chessboard - Print the chessboard
+ * @a: array of pieces
+ *
+ * Return: liothing.
+ */
 void print_chessboard(char (*a)[8])
 {
 	int b, c;
