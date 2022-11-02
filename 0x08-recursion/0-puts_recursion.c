d3e8a46ae731848d94ed4b1ee848ae0b09cf02d5
bkgighello <birukdark@gmail.com>
2022-11-02 10:18:42 -0700

    0 last fix ok

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,18 @@
 
 /**
  * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * @s: string
+ *
+ * Return: On Success 1.
+ * On error, -1 is returned, and error set appropriately.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == 0)
+	{
 		_putchar('\n');
 		return;
-	else
-	{
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
 	}
+		_putchar(*s);
+		_puts_recursion(s + 1);
 }
