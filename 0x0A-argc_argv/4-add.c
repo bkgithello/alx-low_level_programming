c0286829cb201d4908ad4a73ffd504a174849494
bkgighello <birukdark@gmail.com>
2022-11-04 05:01:22 -0700

    4 fixes final

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,29 @@
-#include <stdio.h>                                                                                                                   
-#include <stdlib.h>                                                                                                                  
-#include <ctype.h>                                                                                                                   
-#include <string.h>                                                                                                                  
-                                                                                                                                     
-/**                                                                                                                                  
- * check_num - check - string there are digit                                                                                        
- * @str: array str                                                                                                                   
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                                  
-                                                                                                                                     
-int check_num(char *str)                                                                                                             
-                                                                                                                                     
-{                                                                                                                                    
-/*Declaring variables*/                                                                                                              
-unsigned int count;                                                                                                                  
-                                                                                                                                     
-count = 0;                                                                                                                           
-while (count < strlen(str)) /*count string*/                                                                                         
-                                                                                                                                     
-{                                                                                                                                    
-if (!isdigit(str[count])) /*check if str there are digit*/
-{                                                                                                                                    
-return (0);                                                                                                                          
-}                                                                                                                                    
-                                                                                                                                     
-count++;                                                                                                                             
-}                                                                                                                                    
-return (1);                                                                                                                          
-}                                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * main - Print the name of the program                                                                                              
- * @argc: Count arguments                                                                                                            
- * @argv: Arguments                                                                                                                  
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                                  
-                                                                                                                                     
-int main(int argc, char *argv[])  
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 1)
+		return (0);
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+} 
