637ede4718e82019113208f5781ac04247b918a1
bkgighello <birukdark@gmail.com>
2022-11-07 17:19:19 -0800

    1 fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,25 +6,35 @@
  * @str: char
  * Return: 0
  */
-char *_strdup(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, end;
-	char *array;
+    char *concat;
+    unsigned int len = n, index;
 
-	if (str == NULL)
-		return (NULL);
+    if (s1 == NULL)
+        s1 = "";
 
-	for (end = 0; end <= *str; end++)
-	{
-	}
+    if (s2 == NULL)
+        s2 = "";
 
-	end += 1;
-	array = malloc(sizeof(char) * end);
+    for (index = 0; s1[index]; index++)
+        len++;
 
-	for (i = 0; i < end; i++)
-		array[i] = str[i];
+    concat = malloc(sizeof(char) * (len + 1));
 
-	if (array == NULL)
-		return (NULL);
-	return (array);
+    if (concat == NULL)
+        return (NULL);
+
+    len = 0;
+
+    for (index = 0; s1[index]; index++)
+        concat[len++] = s1[index];
+
+    for (index = 0; s2[index] && index < n; index++)
+        concat[len++] = s2[index];
+
+    concat[len] = '\0';
+
+    return (concat);
 }
+
